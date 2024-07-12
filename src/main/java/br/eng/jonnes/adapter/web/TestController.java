3ace63f8b8d47869f3936faa72995bec342d311e
Jonnes Nascimento <jonnesfn@ipt.br>
2024-07-12 16:51:48 -0300

    Pequenos ajustes

--- src/main/java/br/eng/jonnes/adapter/web/TestController.java
+++ src/main/java/br/eng/jonnes/adapter/web/TestController.java
@@ -1,18 +1,13 @@
 package br.eng.jonnes.adapter.web;
 
-import br.eng.jonnes.core.domain.model.Friend;
-import br.eng.jonnes.core.domain.model.FriendCheckMajorityRequest;
-import br.eng.jonnes.core.domain.model.FriendRegisterRequest;
-import br.eng.jonnes.core.port.in.CheckFriendMajorityInputPort;
-import br.eng.jonnes.core.port.in.RegisterFriendInputPort;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/")
